8df26014fa5f2f5434bcce515eacb2b5473c1c2b
PakeezaB <pakeezabibi17@gmail.com>
2024-06-22 02:01:13 +0500

    Add files via upload

--- main.dart
+++ main.dart
@@ -1,19 +1,42 @@
 import 'package:flutter/material.dart';
 
-class MyStatelessWidget extends StatelessWidget {
-  const MyStatelessWidget({super.key});
+class MyStatefulWidget extends StatefulWidget {
+  const MyStatefulWidget({super.key});
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _MyStatefulWidgetState createState() => _MyStatefulWidgetState();
+}
+
+class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+  String _text = 'This is a stateful widget';
+
+  void _changeText() {
+    setState(() {
+      _text = 'The text has been changed!';
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Stateless Widget Example'),
+        title: const Text('Stateful Widget Example'),
       ),
-      body: const Center(
-        child: Text('This is a stateless widget'),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(_text),
+            ElevatedButton(
+              onPressed: _changeText,
+              child: const Text('Change Text'),
+            ),
+          ],
+        ),
       ),
     );
   }
 }
 
-void main() => runApp(const MaterialApp(home: MyStatelessWidget()));
+void main() => runApp(const MaterialApp(home: MyStatefulWidget()));
